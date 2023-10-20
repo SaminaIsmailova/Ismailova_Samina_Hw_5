5d7a2266684ab36ffbc3c118d13be5b79e91ab98
SaminaIsmailova <samina.ismailova.06@gmail.com>
2023-10-20 19:01:24 +0600

    Initial commit

---
+++ app/src/main/java/com/example/ismailova_samina_hw_5/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ismailova_samina_hw_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
